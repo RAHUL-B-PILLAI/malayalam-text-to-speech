d65f5f9fe171cae03f04d5242615cdc1f797cdbb
Rahul B Pillai <43303036+RAHUL-B-PILLAI@users.noreply.github.com>
2020-12-14 14:47:45 +0530

    Update tesseractOcr.py

--- ocr/tesseractOcr.py
+++ ocr/tesseractOcr.py
@@ -1,4 +1,4 @@
 import pytesseract as ocr
 
-img= ocr.image_to_string('/home/rahul/Desktop/work folder/malayalam text to speech/ocr/download.jpeg',lang='mal')
+img= ocr.image_to_string('/ocr/download.jpeg',lang='mal')
 print(img)
